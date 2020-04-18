425be5826be63a8f020071597dae3bc2a2cd442c
Julius Hietala <julius@smartly.io>
2020-04-18 14:21:21 +0300

    Drop empty tweets

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -55,8 +55,9 @@ def preprocess_text(dataset_path, stem=False):
 
     df.text = df.text.apply(lambda x: preprocess(x, stop_words, stem=stem, stemmer=stemmer))
 
-    print(f"Preprocessing results in empty tweets. How do we drop empty sentences from dataset?")
-    # How to drop tweets which don't have any words left after processing? dropna does not work
+    print(f"Preprocessing results in empty tweets. Dropping empty sentences")
+    nan_value = float("NaN")
+    df.replace("", nan_value, inplace=True)
     df.dropna(axis=0, inplace=True)
 
     print(f"Splitting data")
