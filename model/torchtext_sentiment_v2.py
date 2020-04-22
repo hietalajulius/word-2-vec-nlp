8cdc8a711554f520a23c56df63d35e2263fa8b1a
Jarkko Tulensalo <jarkko.tulensalo@hotmail.com>
2020-04-22 12:32:13 +0300

    confusion matrix plot edit

--- model/torchtext_sentiment_v2.py
+++ model/torchtext_sentiment_v2.py
@@ -32,7 +32,7 @@ def binary_accuracy(preds, y):
     return acc
 
 
-def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'):
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', show_plot=False):
 
 
     cmap = plt.cm.Blues
@@ -43,22 +43,23 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
         print('Confusion matrix, without normalization')
 
     print(cm)
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+    if show_plot:
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
 
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.show()
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.show()
 
 
 def confusion_matrix(model, iterator, device):
